8d6a0be69ae7dac9f0c3469984cf11e9b8895139
Toby-Mills <Mills.toby@gmail.com>
2017-10-18 15:13:57 +0200

    New work on the Event Card component

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { OEventService } from '../../../penoc-sdk/services/oevent.service';
 import { OEventModel } from '../../../penoc-sdk/models/oevent.model';
 
@@ -9,17 +9,11 @@ import { OEventModel } from '../../../penoc-sdk/models/oevent.model';
   styleUrls: ['./event-card.component.css']
 })
 export class EventCardComponent implements OnInit {
-  private oevent: OEventModel;
+  @Input() oevent: OEventModel;
   public constructor(public oeventService: OEventService) { }
 
   ngOnInit() {
 
     }
-
-  public getEvent() {
-    this.oeventService.getOEvent(828).then(response => response.subscribe(data => {
-          this.oevent = data.json()[0];
-          console.log(data.json());
-    }));
-  }
+    
 }
