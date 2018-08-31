9adf51940a81f50f5832de9faa8d61677ab853a1
Toby MIlls <Mills.toby@gmail.com>
2018-08-31 15:19:18 +0200

    Event Results Card binding to OEventResultsSummary model

--- src/app/components/event-results-card/event-results-card.component.ts
+++ src/app/components/event-results-card/event-results-card.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OEventService } from '../../../penoc-sdk/services/oevent.service';
+import { OEventResultSummaryModel } from '../../../penoc-sdk/models/oevent-result-summary.model';
 
 @Component({
+  moduleId: module.id,
   selector: 'penoc-event-results-card',
   templateUrl: './event-results-card.component.html',
   styleUrls: ['./event-results-card.component.css']
 })
 export class EventResultsCardComponent implements OnInit {
+  @Input() oeventSummary: OEventResultSummaryModel;
 
-  constructor() { }
+  public eventResultSummary: OEventResultSummaryModel;
+
+  constructor(public oEventService: OEventService) {
+   }
 
   ngOnInit() {
+    this.oEventService.getOEventResultSummary(666)
+    .then(data => {
+      data.subscribe(response => {
+        this.eventResultSummary = response.json()[0];
+        console.log(this.eventResultSummary);
+      });
+    });
   }
 
 }
