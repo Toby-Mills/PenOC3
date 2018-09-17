9e5e9ed44f6541581e530ca385aadd5efc7a9da4
Toby MIlls <Mills.toby@gmail.com>
2018-09-17 17:07:37 +0200

    loading events on calendar page

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OEventModel } from '../../../penoc-sdk/models/oevent.model';
+import { OEventService } from '../../../penoc-sdk/services/oevent.service';
 
 @Component({
   selector: 'penoc-calendar',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+private oevents: OEventModel[];
 
-  constructor() { }
+  constructor(public oeventService: OEventService) { 
+
+  }
 
   ngOnInit() {
+    this.oeventService.getOEvent(null, null, null, new Date(), null).then(data => {
+      data.subscribe(response =>{
+        this.oevents = response.json();
+      })
+    })
   }
 
 }
