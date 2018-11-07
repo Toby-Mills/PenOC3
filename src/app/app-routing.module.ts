075995c43b3c21d5e86121c8633fd21cbd07a58c
Toby MIlls <Mills.toby@gmail.com>
2018-11-07 09:16:34 +0200

    Added routing to Results page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,17 @@ import { EventResultsComponent } from './components/event-results/event-results.
 import { WhatIsComponent } from './components/what-is/what-is.component';
 import { AboutTheClubComponent } from './components/about-the-club/about-the-club.component';
 import { NewsItemComponent } from './components/news-item/news-item.component';
+import { ResultsComponent } from './components/results/results.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'calendar', component: CalendarComponent},
   {path: 'event-notices/:id', component: EventNoticeComponent},
   {path: 'event-results/:id', component: EventResultsComponent},
+  {path: 'results', component: ResultsComponent},
   {path: 'news/:id', component: NewsItemComponent},
   {path: 'what-is', component: WhatIsComponent},
-  {path: 'about-the-club', component: AboutTheClubComponent}
+  {path: 'about-the-club', component: AboutTheClubComponent},
 ]
 
 @NgModule({
