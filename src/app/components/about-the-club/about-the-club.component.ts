3f490aa6cc69a281f03fe1e788c387ddf68eff2b
Toby MIlls <Mills.toby@gmail.com>
2018-12-14 14:09:44 +0200

    Updates to AboutTheClub component, using environment variables

--- src/app/components/about-the-club/about-the-club.component.ts
+++ src/app/components/about-the-club/about-the-club.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'penoc-about-the-club',
@@ -7,10 +8,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AboutTheClubComponent implements OnInit {
 @Input() summary: boolean = false;
+private env = environment;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
