2b3e252bc57465040a108c767893587500a671b2
Toby MIlls <Mills.toby@gmail.com>
2018-12-14 14:42:32 +0200

    Deleted unused models

--- models/course-result-summary.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var CourseResultSummaryModel = /** @class */ (function () {
-    function CourseResultSummaryModel() {
-    }
-    return CourseResultSummaryModel;
-}());
-exports.CourseResultSummaryModel = CourseResultSummaryModel;
