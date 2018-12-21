0a44b6d975fc41f6db6e8bac6a2edfe669868258
Toby MIlls <Mills.toby@gmail.com>
2018-12-21 15:25:57 +0200

    removed unused imports

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { OEventService } from 'penoc-sdk/services/oevent.service';
 import { OEventModel } from 'penoc-sdk/models/oevent.model';
 import { OEventResultSummaryModel } from 'penoc-sdk/models/oevent-result-summary.model';
-import { NewsService } from 'penoc-sdk/services/news.service';
 import { NewsModel } from 'penoc-sdk/models/news.model';
 import { HomeService } from '../../services/home.service';
 
@@ -14,7 +12,7 @@ import { HomeService } from '../../services/home.service';
 })
 export class HomeComponent implements OnInit { 
 
-  constructor(public router: Router, public oEventService: OEventService, public newsService: NewsService, public homeService: HomeService) { }
+  constructor(public router: Router, public homeService: HomeService) { }
 
   ngOnInit() {
     if (this.homeService.yearsDataLoaded == 0){
@@ -33,8 +31,6 @@ export class HomeComponent implements OnInit {
       }
     }
   }
-
-  
 
   public calendarEventClick(oevent: OEventModel){
     this.router.navigate(['/event-notices', oevent.id]);
